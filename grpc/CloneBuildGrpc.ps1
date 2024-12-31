355e861d7d83efd3c6a0fece0499e65dc504a169
Moein Verkiani <mr.verkiani@gmail.com>
2024-12-31 13:40:46 +0330

    Update scripts

--- grpc/CloneBuildGrpc.ps1
+++ grpc/CloneBuildGrpc.ps1
@@ -3,15 +3,34 @@ param (
     [Parameter (Mandatory = $true)] [ValidateSet('x86', 'x64')] [string]$BuildArch
 )
 
-function InitDevShell {   
-    $vsPath = &(Join-Path ${env:ProgramFiles(x86)} "\Microsoft Visual Studio\Installer\vswhere.exe") -latest -property installationpath
-    
-    if ($BuildArch -ieq "x86") {
-        & (Join-Path -Path $vsPath -ChildPath "Common7\Tools\Launch-VsDevShell.ps1") -Arch x86 -HostArch amd64 -SkipAutomaticLocation
+function GetVcpkgPath() {
+    $vcpkg_install_dir = ""
+    #
+    # Check whther vcpkg is already installed/integrated or not, in case there was no vcpkg installed, then
+    # try to clone and installed at hardcoded address "c:\dev\kits\vcpkg"
+    #
+    if (Test-Path -Path $env:localappdata\vcpkg\vcpkg.path.txt) {
+        $vcpkg_install_dir = Get-Content $env:localappdata\vcpkg\vcpkg.path.txt
     }
-    if ($BuildArch -ieq "x64") {
-        & (Join-Path -Path $vsPath -ChildPath "Common7\Tools\Launch-VsDevShell.ps1") -Arch amd64 -HostArch amd64 -SkipAutomaticLocation
-    } 
+
+    if ( ([string]::IsNullOrEmpty($vcpkg_install_dir))  ) {
+        $vcpkg_install_dir = "c:\dev\kits\vcpkg"
+    }
+    
+    return $vcpkg_install_dir
+}
+
+function InitDevShell {   
+    if (-not $env:VSCMD_VER) {
+        $vsPath = &(Join-Path ${env:ProgramFiles(x86)} "\Microsoft Visual Studio\Installer\vswhere.exe") -latest -property installationpath
+        
+        if ($BuildArch -ieq "x86") {
+            & (Join-Path -Path $vsPath -ChildPath "Common7\Tools\Launch-VsDevShell.ps1") -Arch x86 -HostArch amd64 -SkipAutomaticLocation
+        }
+        if ($BuildArch -ieq "x64") {
+            & (Join-Path -Path $vsPath -ChildPath "Common7\Tools\Launch-VsDevShell.ps1") -Arch amd64 -HostArch amd64 -SkipAutomaticLocation
+        } 
+    }
 }
 
 function CloneRepo {
@@ -31,23 +50,6 @@ function CloneRepo {
     }
 }
 
-function EnsureDirectoryExists {
-    param (
-        [Parameter(Mandatory=$true)] [string]$DirectoryPath
-    )
-    
-    if (-not (Test-Path $DirectoryPath)) {
-        try {
-            New-Item -ItemType Directory -Force -Path $DirectoryPath | Out-Null
-            Write-Host "Created directory: $DirectoryPath"
-        }
-        catch {
-            Write-Host "Error creating directory $DirectoryPath: $_" -ForegroundColor Red
-            exit 1
-        }
-    }
-}
-
 function CmakeGenerate {
     param (
         [Parameter (Mandatory = $true)] [string]$SourceDir,
@@ -56,9 +58,34 @@ function CmakeGenerate {
         [Parameter (Mandatory = $true)] [string]$InstallDir
     )
     
-    & cmake.exe -G Ninja -S "${SourceDir}" -B "${BuildDir}" -DCMAKE_BUILD_TYPE="${BuildConfig}" -DCMAKE_INSTALL_PREFIX="${InstallDir}" -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe"
-}
 
+    $vcpkg_path = GetVcpkgPath
+    if ( -Not (Test-Path -Path $vcpkg_path)) {
+        Write-Error Failed to locate vcpkg.
+    }
+
+
+    # Do not compile redundant plugins: Node, Objective-C, PHP, Python and Ruby
+    # gRPC_BACKWARDS_COMPATIBILITY_MODE Build libraries that are binary compatible across a larger number of OS and libc versions 
+    # Build gRPC++ with OpenSSL instead of BoringSSL
+    # -DgRPC_SSL_PROVIDER=package
+    & cmake.exe -G Ninja -S "${SourceDir}" -B "${BuildDir}" -DCMAKE_BUILD_TYPE="${BuildConfig}" -DCMAKE_INSTALL_PREFIX="${InstallDir}" `
+
+        -DgRPC_BUILD_GRPC_CSHARP_PLUGIN=OFF -DgRPC_BUILD_GRPC_NODE_PLUGIN=OFF -DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF `
+
+        -DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF  -DgRPC_BUILD_GRPC_PYTHON_PLUGIN=OFF -DgRPC_BUILD_GRPC_RUBY_PLUGIN=OFF  `
+
+        -DgRPC_BACKWARDS_COMPATIBILITY_MODE=ON -DgRPC_SSL_PROVIDER=package `
+
+        -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe" `
+
+        -DCMAKE_TOOLCHAIN_FILE="${vcpkg_path}\scripts\buildsystems\vcpkg.cmake" -DVCPKG_INSTALLED_DIR="C:\vcpkg_install_${BuildArch}" -DVCPKG_TARGET_TRIPLET="${BuildArch}-windows-v143"
+        
+    # To build the gRPC with MT
+    #  -DgRPC_MSVC_STATIC_RUNTIME=ON
+    # 
+}
+    
 function CmakeBuildInstall {
     param (
         [Parameter (Mandatory = $true)] [string]$SourceDir,
@@ -72,19 +99,25 @@ function CmakeBuildInstall {
 
 InitDevShell
 
-$Version = "1.59.1"
+$Version = "1.67.0"
 $BuildRepoDir = "C:\dev\source"
 
-EnsureDirectoryExists -DirectoryPath $BuildRepoDir
+$SourceRepoDir = "${BuildRepoDir}\grpc_${Version}"
+$BuildDir = "$SourceRepoDir\${BuildArch}_${BuildConfig}_build"
+$InstallDir = "C:\dev\third_party\grpc\build-${BuildArch}-${BuildConfig}"
 
-$SourceRepoDir 	= "${BuildRepoDir}\grpc_${Version}"
-$BuildDir 		= "$SourceRepoDir\${BuildArch}_release_build"
-$InstallDir 	= "C:\dev\third_party\grpc\build-${BuildArch}-${BuildConfig}"
-
-EnsureDirectoryExists -DirectoryPath $SourceRepoDir
-EnsureDirectoryExists -DirectoryPath $BuildDir
-EnsureDirectoryExists -DirectoryPath $InstallDir
+Write-Host --------------------------------------------------------------------
+Write-Host "Version:       ${Version}"
+Write-Host "BuildRepoDir:  ${BuildRepoDir}"
+Write-Host "SourceRepoDir: ${SourceRepoDir}"
+Write-Host "BuildDir:      ${BuildDir}"
+Write-Host "InstallDir:    ${InstallDir}"
+Write-Host --------------------------------------------------------------------
 
 CloneRepo -RepoDir "${SourceRepoDir}" -TargetBranch "${Version}"
 CmakeGenerate -SourceDir "${SourceRepoDir}" -BuildDir "${BuildDir}" -BuildConfig "${BuildConfig}" -InstallDir "${InstallDir}"
 CmakeBuildInstall -SourceDir "${SourceRepoDir}" -BuildDir "${BuildDir}" -BuildConfig "${BuildConfig}" -InstallDir "${InstallDir}"
+
+# Remove the zlib.lib to remove extra dependency to the zlib.dll.
+$ZlibFullPath = (Join-Path -Path "${InstallDir}" -ChildPath "lib\zlib.lib")
+if( Test-Path -Path "${ZlibFullPath}") { Remove-Item -Path "${ZlibFullPath}" }
